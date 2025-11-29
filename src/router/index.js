078625b0344636a350a1c7d5602e8c540e967966
Juwoojae <juwqq1234@naver.com>
2025-11-30 00:11:15 +0900

    회원가입

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'
-
-const routes = []
+import MemberCreate from "@/views/MemberCreate.vue"
+//router 는 ssr 로 따지면 controller 임 view 를 띄워주는 역할을 한다 /home 엔드 포인트 -> homePage 라는  view
+//view 는 Vue Component 에 대응된다
+const routes = [
+    {
+        path: '/member/create',
+        name: 'MemberCreate',
+        component: MemberCreate
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
---
+++ src/views/MemberCreate.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-row justify="center" >
+      <v-col cols="12" sm="4" md="6">
+        <v-card>
+          <v-card-title class="test-h5 text-center">회원가입</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="memberCreate">
+              <v-text-field
+                label="이름"
+                v-model="name"
+                aria-required="true"
+              >
+              </v-text-field>
+              <v-text-field
+                  label="email"
+                  v-model="email"
+                  type="email"
+                  aria-required="true"
+              >
+              </v-text-field>
+              <v-text-field
+                  label="password"
+                  v-model="password"
+                  type="password"
+                  aria-required="true"
+              >
+              </v-text-field>
+              <v-btn type="submit" color="primary" block>등록</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      name: "",
+      email: "",
+      password: "",
+    }
+  },
+  methods: {
+    async memberCreate(){
+      //이렇게 하면 자동으로 json 으로 변환
+      const data = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      }
+      //axios 는 시간이 좀 걸려서 비동기로 주로 씀
+      await axios.post("http://localhost:8080/member/create",data);
+      this.$router.push({ path: "/" });
+    }
+  }
+
+}
+</script>
