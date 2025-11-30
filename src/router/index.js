ca6251cb38bb7b50664f1c9bf47b754e02c899a1
Juwoojae <juwqq1234@naver.com>
2025-11-30 15:01:54 +0900

    로그아웃 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import MemberCreate from "@/views/MemberCreate.vue"
+import LoginPage from "@/views/LoginPage.vue";
 //router 는 ssr 로 따지면 controller 임 view 를 띄워주는 역할을 한다 /home 엔드 포인트 -> homePage 라는  view
 //view 는 Vue Component 에 대응된다
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
         path: '/member/create',
         name: 'MemberCreate',
         component: MemberCreate
+    },
+    {
+        path: '/login',
+        name: 'LoginPage',
+        component: LoginPage
     }
 ]
 
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,16 +3,9 @@
     <v-row justify="center" >
       <v-col cols="12" sm="4" md="6">
         <v-card>
-          <v-card-title class="test-h5 text-center">회원가입</v-card-title>
+          <v-card-title class="test-h5 text-center">로그인</v-card-title>
           <v-card-text>
-            <v-form @submit.prevent="memberCreate">
-              <v-text-field
-                label="이름"
-
-                v-model="name"
-                aria-required="true"
-              >
-              </v-text-field>
+            <v-form @submit.prevent="doLogin">
               <v-text-field
                   label="email"
                   v-model="email"
@@ -27,7 +20,7 @@
                   aria-required="true"
               >
               </v-text-field>
-              <v-btn type="submit" color="primary" block>등록</v-btn>
+              <v-btn type="submit" color="primary" block>로그인</v-btn>
             </v-form>
           </v-card-text>
         </v-card>
@@ -38,25 +31,22 @@
 
 <script>
 import axios from "axios";
+
 export default {
   data(){
     return{
-      name: "",
       email: "",
       password: "",
     }
   },
-  methods: {
-    async memberCreate(){
-      //이렇게 하면 자동으로 json 으로 변환
-      const data = {
-        name: this.name,
-        email: this.email,
-        password: this.password,
-      }
-      //axios 는 시간이 좀 걸려서 비동기로 주로 씀
-      await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member/create`,data);
-      this.$router.push({ path: "/" });
+  methods:{
+    async doLogin(){
+      const data = {email: this.email, password: this.password,}
+      const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member/doLogin`,data);
+      console.log(response); //response.data 에 토큰이 들어있음을 확인
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      window.location.href="/"; //성공하면 루트로 이동
     }
   }
 
